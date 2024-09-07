42a20222ab66410346e0546b377efc9752d2dcd4
Kostya Farber <kostya.farber@gmail.com>
2024-09-07 09:46:58 +0100

    get started on essentials bundle, improve bootstrap script

--- boostrap.sh
+++ boostrap.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-cat .bootstrap.ascii 
+cat << "EOF"
+    ____              __       __                 
+   / __ )____  ____  / /______/ /__________ _____ 
+  / __  / __ \/ __ \/ __/ ___/ __/ ___/ __ `/ __ \
+ / /_/ / /_/ / /_/ / /_(__  ) /_/ /  / /_/ / /_/ /
+/_____/\____/\____/\__/____/\__/_/   \__,_/ .___/ 
+                                         /_/      
+EOF
 
 sleep 2
 
@@ -11,4 +18,12 @@ echo "boostrapping system..."
 echo "installing brew..."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "installing oh-my-zsh..."
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "cloning dotfiles.."
+
+git clone https://github.com/kostyafarber/dotfiles.git .dotfiles
 
