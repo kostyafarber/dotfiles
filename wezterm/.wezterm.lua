7a2ebb973f6c9f7d647a8c40025fe6644f30ce8c
Kostya Farber <kostya.farber@gmail.com>
2024-09-13 21:11:49 +0100

    improve wezterm lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local mux = wezterm.mux
 
 config.font = wezterm.font_with_fallback {
     'Berkeley Mono', 'Hasklug Nerd Font Mono'
-}
+    }
 
 config.enable_tab_bar = false
 config.default_prog = {
@@ -34,7 +34,7 @@ config.macos_window_background_blur = 10
 config.default_cursor_style = 'SteadyUnderline'
 
 wezterm.on('gui-startup', function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
+    local _, _, window = mux.spawn_window(cmd or {})
     window:gui_window():maximize()
 end)
 
