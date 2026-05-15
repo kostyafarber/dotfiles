baeb572525323938f0e8b0708e316d3aa081a7d1
Kostya Farber <kostya.farber@gmail.com>
2026-05-15 17:30:20 +0100

    fix

--- nvim/.config/nvim/lua/plugins/nvim/formatter.lua
+++ nvim/.config/nvim/lua/plugins/nvim/formatter.lua
@@ -14,11 +14,28 @@ return {
 	},
 	config = function()
 		local conform = require("conform")
+		local util = require("conform.util")
 
 		conform.formatters.templ = {
 			inherit = false,
 			command = "templ",
 			args = { "fmt", "-stdout", "-stdin-filepath", "$FILENAME" },
+		}
+
+		conform.formatters.eslint_d = {
+			cwd = util.root_file({
+				".eslintrc",
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.json",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+				"eslint.config.js",
+				"eslint.config.mjs",
+				"eslint.config.cjs",
+				"eslint.config.ts",
+			}),
+			require_cwd = true,
 		}
 
 		conform.setup({
@@ -30,19 +47,19 @@ return {
 				javascriptreact = { "oxfmt", "prettierd", stop_after_first = true },
 				typescript = { "oxfmt", "prettierd", stop_after_first = true },
 				typescriptreact = { "oxfmt", "prettierd", stop_after_first = true },
-				html = { "prettierd" },
-				css = { "prettierd" },
+				html = { "oxfmt", "prettierd", stop_after_first = true },
+				css = { "oxfmt", "prettierd", stop_after_first = true },
 				go = { "gofumpt", "goimports" },
 				json = { "jq" },
 			},
 			default_format_opts = {
 				lsp_format = "never",
 			},
-			format_on_save = function(bufnr)
+			format_after_save = function(bufnr)
 				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 					return
 				end
-				return { timeout_ms = 3000 }
+				return { lsp_format = "never" }
 			end,
 		})
 
